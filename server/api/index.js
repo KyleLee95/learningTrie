d85cd414f52e18d22d638f9f21a72fcf6ef6f330
Kyle Lee <Kylecrlee95@gmail.com>
2019-05-28 21:31:56 -0500

    fixed some of the db issues. tbh, everything is working so I guess there's not really much of an issue of just continuing what I'm doing

--- server/api/index.js
+++ server/api/index.js
@@ -9,6 +9,8 @@ router.use('/resources', require('./resources'))
 router.use('/reviews', require('./reviews'))
 router.use('/tags', require('./tags'))
 router.use('/comments', require('./comments'))
+router.use('/message', require('./message'))
+router.use('/conversation', require('./conversation'))
 
 router.use((req, res, next) => {
   const error = new Error('Not Found')
