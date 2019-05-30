b144b48854558f339e5b80ca6067a4746495866a
Kyle Lee <Kylecrlee95@gmail.com>
2019-05-30 15:24:40 -0500

    commit before npm install moment

--- server/db/models/comment.js
+++ server/db/models/comment.js
@@ -4,6 +4,12 @@ const db = require('../db')
 const Comment = db.define('comment', {
   content: {
     type: Sequelize.TEXT
+  },
+  DateTime: {
+    type: Sequelize.DATEONLY,
+    get: function() {
+      return moment(this.getDataValue('DateTime')).format('DD.MM.YYYY')
+    }
   }
 })
 
