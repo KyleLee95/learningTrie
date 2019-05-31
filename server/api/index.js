305915ab981263ab5303bdf1915c2ff9be101d93
Kyle Lee <Kylecrlee95@gmail.com>
2019-05-31 13:31:04 -0500

    commit before building message system

--- server/api/index.js
+++ server/api/index.js
@@ -9,9 +9,10 @@ router.use('/resources', require('./resources'))
 router.use('/reviews', require('./reviews'))
 router.use('/tags', require('./tags'))
 router.use('/comments', require('./comments'))
-router.use('/message', require('./message'))
+router.use('/messages', require('./message'))
 router.use('/conversation', require('./conversation'))
 router.use('/links', require('./links'))
+
 router.use((req, res, next) => {
   const error = new Error('Not Found')
   error.status = 404
--- server/api/message.js
+++ server/api/message.js
@@ -1,3 +1,11 @@
 const router = require('express').Router()
-const {Message, User} = require('../db/models')
+const {Message, User, Conversation} = require('../db/models')
 module.exports = router
+
+router.post('/', async (req, res, next) => {
+  try {
+    res.status(200).send('A')
+  } catch (err) {
+    next(err)
+  }
+})
