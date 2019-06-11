a0901d89ef5bdd946fe591220beaadba52cb7bc1
Kyle Lee <Kylecrlee95@gmail.com>
2019-06-11 13:23:02 -0500

    implemented rendering logic for bold lineitems inbox based on read/unread status of conversations

--- client/components/InboxLineItem.js
+++ client/components/InboxLineItem.js
@@ -5,32 +5,122 @@ import moment from 'moment'
 export const InboxLineItem = props => {
   console.log(props)
   return (
-    <li
-      id={`${props.conversation.id.toString()}`}
-      key={props.conversation.id}
-      style={{fontWeight: 'bold'}}
-      onClick={() => props.toggleRead(props.conversation.id.toString())}
-    >
-      <Card>
-        <Link
-          to={`/conversation/${props.conversation.id}`}
-          style={{textDecoration: 'none'}}
+    <React.Fragment>
+      {props.conversation.sender === props.user.username ? (
+        props.conversation.senderRead === true ? (
+          <li
+            id={`${props.conversation.id.toString()}`}
+            key={props.conversation.id}
+            style={{fontWeight: 'none'}}
+            onClick={() => props.toggleRead(props.conversation.id.toString())}
+          >
+            <Card>
+              <Link
+                to={`/conversation/${props.conversation.id}`}
+                style={{textDecoration: 'none'}}
+              >
+                <Row>
+                  <Col lg={{span: 3, offset: 1}}>
+                    {props.conversation.sender === props.user.username
+                      ? props.conversation.receiver
+                      : props.conversation.sender}{' '}
+                  </Col>
+                  <Col lg={5}>{props.conversation.subject}</Col>
+                  <Col lg={3}>
+                    {moment(props.conversation.createdAt).format(
+                      'MMMM Do YYYY, h:mm:ss a'
+                    )}
+                  </Col>
+                </Row>
+              </Link>
+            </Card>
+          </li>
+        ) : (
+          <li
+            id={`${props.conversation.id.toString()}`}
+            key={props.conversation.id}
+            style={{fontWeight: 'bold'}}
+            onClick={() => props.toggleRead(props.conversation.id.toString())}
+          >
+            <Card>
+              <Link
+                to={`/conversation/${props.conversation.id}`}
+                style={{textDecoration: 'none'}}
+              >
+                <Row>
+                  <Col lg={{span: 3, offset: 1}}>
+                    {props.conversation.sender === props.user.username
+                      ? props.conversation.receiver
+                      : props.conversation.sender}{' '}
+                  </Col>
+                  <Col lg={5}>{props.conversation.subject}</Col>
+                  <Col lg={3}>
+                    {moment(props.conversation.createdAt).format(
+                      'MMMM Do YYYY, h:mm:ss a'
+                    )}
+                  </Col>
+                </Row>
+              </Link>
+            </Card>
+          </li>
+        )
+      ) : props.conversation.receiverRead === true ? (
+        <li
+          id={`${props.conversation.id.toString()}`}
+          key={props.conversation.id}
+          style={{fontWeight: 'none'}}
+          onClick={() => props.toggleRead(props.conversation.id.toString())}
         >
-          <Row>
-            <Col lg={{span: 3, offset: 1}}>
-              {props.conversation.sender === props.user.username
-                ? props.conversation.receiver
-                : props.conversation.sender}{' '}
-            </Col>
-            <Col lg={5}>{props.conversation.subject}</Col>
-            <Col lg={3}>
-              {moment(props.conversation.createdAt).format(
-                'MMMM Do YYYY, h:mm:ss a'
-              )}
-            </Col>
-          </Row>
-        </Link>
-      </Card>
-    </li>
+          <Card>
+            <Link
+              to={`/conversation/${props.conversation.id}`}
+              style={{textDecoration: 'none'}}
+            >
+              <Row>
+                <Col lg={{span: 3, offset: 1}}>
+                  {props.conversation.sender === props.user.username
+                    ? props.conversation.receiver
+                    : props.conversation.sender}{' '}
+                </Col>
+                <Col lg={5}>{props.conversation.subject}</Col>
+                <Col lg={3}>
+                  {moment(props.conversation.createdAt).format(
+                    'MMMM Do YYYY, h:mm:ss a'
+                  )}
+                </Col>
+              </Row>
+            </Link>
+          </Card>
+        </li>
+      ) : (
+        <li
+          id={`${props.conversation.id.toString()}`}
+          key={props.conversation.id}
+          style={{fontWeight: 'bold'}}
+          onClick={() => props.toggleRead(props.conversation.id.toString())}
+        >
+          <Card>
+            <Link
+              to={`/conversation/${props.conversation.id}`}
+              style={{textDecoration: 'none'}}
+            >
+              <Row>
+                <Col lg={{span: 3, offset: 1}}>
+                  {props.conversation.sender === props.user.username
+                    ? props.conversation.receiver
+                    : props.conversation.sender}{' '}
+                </Col>
+                <Col lg={5}>{props.conversation.subject}</Col>
+                <Col lg={3}>
+                  {moment(props.conversation.createdAt).format(
+                    'MMMM Do YYYY, h:mm:ss a'
+                  )}
+                </Col>
+              </Row>
+            </Link>
+          </Card>
+        </li>
+      )}
+    </React.Fragment>
   )
 }
