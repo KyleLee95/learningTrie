c34f8d6d96e6ee477683e850867b15f7f639e896
Kyle Lee <Kylecrlee95@gmail.com>
2019-07-30 16:59:50 -0500

    eat my dick boxes

--- client/components/SignUp.js
+++ client/components/SignUp.js
@@ -23,108 +23,121 @@ class SignUp extends Component {
   render() {
     const {name, displayName, handleSubmit, error} = this.props
     return (
-      // <Row>
-      <React.Fragment>
-        <Row>
-          <Col xs={12} sm={12} md={12} lg={6} xl={6}>
-            <Carousel style={{paddingTop: '10%'}}>
-              <Carousel.Item>
-                <img
-                  className="d-block w-100"
-                  src="/carousel0.png"
-                  alt="Welcome"
-                />
-                {/* <Carousel.Caption> */}
-                {/* <h3 style={{color: 'black'}}>🌎pen Source Ed</h3>
+      <div>
+        <Row style={{padding: '0'}}>
+          <Col xs={12} style={{padding: '0'}}>
+            <Jumbotron style={{height: '100vh', margin: '0'}}>
+              <Row>
+                <Col
+                  xs={12}
+                  sm={12}
+                  md={12}
+                  lg={6}
+                  xl={6}
+                  style={{paddLeft: '5%'}}
+                >
+                  <Carousel style={{paddingTop: '10%'}}>
+                    <Carousel.Item>
+                      <img
+                        className="d-block w-100"
+                        src="/carousel0.png"
+                        alt="Welcome"
+                      />
+                      {/* <Carousel.Caption> */}
+                      {/* <h3 style={{color: 'black'}}>🌎pen Source Ed</h3>
                 <p style={{color: 'black'}}>
                   A community driven learning platform and visualization tool
                   that helps you see what you're learning.
                 </p>
                 </Carousel.Caption> */}
-              </Carousel.Item>
-              <Carousel.Item>
-                <img
-                  className="d-block w-100"
-                  src="/carousel1.png"
-                  alt="First slide"
-                />
-              </Carousel.Item>
-              <Carousel.Item>
-                <img
-                  className="d-block w-100"
-                  src="/carousel2.png"
-                  alt="Second slide"
-                />
-              </Carousel.Item>
-            </Carousel>
-          </Col>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                      <img
+                        className="d-block w-100"
+                        src="/carousel1.png"
+                        alt="First slide"
+                      />
+                    </Carousel.Item>
+                    <Carousel.Item>
+                      <img
+                        className="d-block w-100"
+                        src="/carousel2.png"
+                        alt="Second slide"
+                      />
+                    </Carousel.Item>
+                  </Carousel>
+                </Col>
 
-          <Col xs={12} sm={12} md={12} lg={6} xl={6}>
-            <Row>
-              <Col xs={{span: 10, offset: 1}}>
-                <Form onSubmit={handleSubmit} name={name}>
-                  <Form.Group as={Row}>
-                    <Form.Text style={{fontWeight: 'bold', fontSize: '20pt'}}>
-                      Sign Up
-                    </Form.Text>
-                    <hr />
-                  </Form.Group>
-                  <Form.Group as={Row}>
-                    <Form.Label>Email</Form.Label>
-                    <Form.Control
-                      name="email"
-                      type="Email"
-                      placeholder="Email"
-                    />
-                  </Form.Group>
-                  <Form.Group as={Row}>
-                    <Form.Label>First Name</Form.Label>
-                    <Form.Control
-                      name="firstName"
-                      type="name"
-                      placeholder="First Name"
-                    />
-                  </Form.Group>
+                <Col xs={12} sm={12} md={12} lg={6} xl={6}>
+                  <Row>
+                    <Col xs={{span: 10, offset: 1}}>
+                      <Form onSubmit={handleSubmit} name={name}>
+                        <Form.Group as={Row}>
+                          <Form.Text
+                            style={{fontWeight: 'bold', fontSize: '20pt'}}
+                          >
+                            Sign Up
+                          </Form.Text>
+                          <hr />
+                        </Form.Group>
+                        <Form.Group as={Row}>
+                          <Form.Label>Email</Form.Label>
+                          <Form.Control
+                            name="email"
+                            type="Email"
+                            placeholder="Email"
+                          />
+                        </Form.Group>
+                        <Form.Group as={Row}>
+                          <Form.Label>First Name</Form.Label>
+                          <Form.Control
+                            name="firstName"
+                            type="name"
+                            placeholder="First Name"
+                          />
+                        </Form.Group>
 
-                  <Form.Group as={Row}>
-                    <Form.Label>Last Name</Form.Label>
-                    <Form.Control
-                      name="lastName"
-                      type="name"
-                      placeholder="Last Name"
-                    />
-                  </Form.Group>
-                  <Form.Group as={Row}>
-                    <Form.Label>Username</Form.Label>
-                    <Form.Control
-                      name="username"
-                      type="username"
-                      placeholder="Username"
-                    />
-                  </Form.Group>
-                  <Form.Group as={Row}>
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control
-                      name="password"
-                      type="password"
-                      placeholder="Password"
-                    />
-                  </Form.Group>
-                  <Form.Group as={Row}>
-                    <Button type="submit" variant="primary">
-                      {displayName}
-                    </Button>
-                  </Form.Group>
+                        <Form.Group as={Row}>
+                          <Form.Label>Last Name</Form.Label>
+                          <Form.Control
+                            name="lastName"
+                            type="name"
+                            placeholder="Last Name"
+                          />
+                        </Form.Group>
+                        <Form.Group as={Row}>
+                          <Form.Label>Username</Form.Label>
+                          <Form.Control
+                            name="username"
+                            type="username"
+                            placeholder="Username"
+                          />
+                        </Form.Group>
+                        <Form.Group as={Row}>
+                          <Form.Label>Password</Form.Label>
+                          <Form.Control
+                            name="password"
+                            type="password"
+                            placeholder="Password"
+                          />
+                        </Form.Group>
+                        <Form.Group as={Row}>
+                          <Button type="submit" variant="primary">
+                            {displayName}
+                          </Button>
+                        </Form.Group>
 
-                  {error &&
-                    error.response && <div> {error.response.data} </div>}
-                </Form>
-              </Col>
-            </Row>
+                        {error &&
+                          error.response && <div> {error.response.data} </div>}
+                      </Form>
+                    </Col>
+                  </Row>
+                </Col>
+              </Row>
+            </Jumbotron>
           </Col>
         </Row>
-      </React.Fragment>
-      // </Row>
+      </div>
     )
   }
 }
